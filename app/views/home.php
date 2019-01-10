8c5891305fdcb9e1b74038a5adda53ad972f1946
palanas-jeffrey <palanas.jeffrey@gmail.com>
2019-01-10 16:33:46 +0800

    activity

---
+++ app/views/home.php
@@ -0,0 +1,18 @@
+<?php require_once '../partials/template.php'; ?>
+
+
+<?php function get_page_content() { ?>
+ 
+	<div class="container-fluid">
+		<div class="jumbotron bg-dark">
+			<h1 class="text-center">7meal</h1>
+			<p class="text-center">Masarap kumain lalo na pag nag kamay.</p>
+
+		</div>	
+	</div>
+	
+
+	
+<?php }; ?>
+
+
