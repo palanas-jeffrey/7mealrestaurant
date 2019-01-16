0737768e9ac1ff6a2d3c4e246c8128e5c16a7295
palanas-jeffrey <palanas.jeffrey@gmail.com>
2019-01-16 16:21:22 +0800

    add new item'

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,9 +1,9 @@
-<footer class="row redbg  fixed-bottom px-3 justify-content-center">
+<footer class="row redbg px-3 justify-content-center">
 	<small class=" text-white">
 		DISCLAIMER: This site is for educational purposes only. I do not own nor claim to own any content within this site. &copy;7mealRestaurant Inc.
 	</small>
 	<hr>	
-	<!-- <p>And Jesus said to them, “I am the bread of life. He who comes to Me shall never hunger, and he who believes in Me shall never thirst. -John 6:35 </p> -->
+	<p class="text-white">And Jesus said to them, “I am the bread of life. He who comes to Me shall never hunger, and he who believes in Me shall never thirst. -John 6:35 </p>
 </footer>
 
 
